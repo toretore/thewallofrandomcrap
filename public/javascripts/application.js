467ae6e032e722ce3ac0783e56c20b4116117bd3
Tore Darell <toredarell@gmail.com>
2008-10-18 15:49:25 +0200

    Labelify

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,27 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+StickyBoard = {
+
+  labelify: function(){
+    $$('form p.labelify').each(function(p){
+      var label = p.down('label'),
+          input = p.down('input');
+      label.hide();
+      input.value = label.innerHTML;
+      input.observe('focus', function(){
+        if (input.value == label.innerHTML) {
+          input.value = '';
+        }
+      });
+      input.observe('blur', function(){
+        if (!input.value) {
+          input.value = label.innerHTML;
+        }
+      });
+    });
+  }
+
+};
+
+
+document.observe('dom:loaded', function(){
+  StickyBoard.labelify();
+});

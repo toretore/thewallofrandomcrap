4f178fe7a09fb5c62dfdfaa38ca1a4abb4048dfa
Tore Darell <toredarell@gmail.com>
2008-10-18 20:40:25 +0200

    Symlink twitter.yml on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ namespace :deploy do
   end
 
   task :after_symlink do
-    %w[database.yml].each do |c|
+    %w[database.yml twitter.yml].each do |c|
       run "ln -nsf #{shared_path}/system/config/#{c} #{current_path}/config/#{c}"
     end
 

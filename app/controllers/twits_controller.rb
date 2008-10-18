770b4da535aa65363e136466eb4a332f7cc751d2
Tore Darell <toredarell@gmail.com>
2008-10-18 20:29:25 +0200

    Add index action to TwitsController

--- app/controllers/twits_controller.rb
+++ app/controllers/twits_controller.rb
@@ -1,2 +1,19 @@
 class TwitsController < ApplicationController
+
+
+  def index
+    @twits = if params[:last_id]
+      Twit.by_time(:limit => 20).newer_than(params[:last_id])
+    else
+      Twit.by_time(:limit => 20)
+    end
+
+    if @twits.empty?
+      render :text => ":(", :status => 404
+    else
+      render :partial => "twit", :collection => @twits
+    end
+  end
+
+
 end

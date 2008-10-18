d2090a004c840d741a911435d1e901d577c76bd6
Tore Darell <toredarell@gmail.com>
2008-10-18 20:28:50 +0200

    Add newer_than scope to Twit

--- app/models/twit.rb
+++ app/models/twit.rb
@@ -3,6 +3,7 @@ class Twit < ActiveRecord::Base
   validates_presence_of :text, :screen_name, :name
 
   named_scope :by_time, :order => "twits.twitted_at DESC"
+  named_scope :newer_than, lambda{|id| {:conditions => ["twits.id > ?", id]} }
 
 
   def text_without_reply_name

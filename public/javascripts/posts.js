567a2711bd9fe79b2e46bb862be4a6470ccf32d1
Tore Darell <toredarell@gmail.com>
2008-10-19 16:04:45 +0200

    Hijack delete action

--- public/javascripts/posts.js
+++ public/javascripts/posts.js
@@ -49,13 +49,10 @@ StickyBoard.Post = ActiveElement.Base.spawn('post', {
     this.hijackDeleteForm();
   },
   
-  setHighlightedValue: function(b){
-    this.element[b ? 'addClassName' : 'removeClassName']('highlighted');
-  },
-  
-  getHighlightedValue: function(){
-    return this.element.hasClassName('highlighted');
-  },
+  setHighlightedValue: function(b){ this.element[b ? 'addClassName' : 'removeClassName']('highlighted'); },
+  getHighlightedValue: function(){ return this.element.hasClassName('highlighted'); },
+  setDisabledValue: function(b){ this.element[b ? 'addClassName' : 'removeClassName']('disabled'); },
+  getDisabledValue: function(){ return this.element.hasClassName('disabled'); },
 
   highlight: function(){
     var post = this;
@@ -66,11 +63,52 @@ StickyBoard.Post = ActiveElement.Base.spawn('post', {
   },
 
   hijackDeleteForm: function(){
-    var form = this.element.down('.tools .delete form');
+    var post = this,
+        form = this.element.down('.tools .delete form');
+
+    //Replace form with a link
     var link = new Element('a', {href:'#', 'class':'delete'});
     link.update(form.down('input[type=submit]').value);
+
+    //Observe the link
+    link.observe('click', function(e){
+      e.stop();
+      post.set('highlighted', true);
+      if (confirm('Are you sure you want to delete this post?')) {
+        new Ajax.Request(form.readAttribute('action'), {
+          method: 'delete',
+          parameters: {authenticity_token: form.down('[name=authenticity_token]').value},
+          onSuccess: function(){
+            post.fancyRemove();
+          },
+          onFailure: function(){
+            alert("FAIL");
+          },
+          onComplete: function(){
+            post.set('highlighted', false);
+          }
+        });
+      } else {
+        post.set('highlighted', false);
+      }
+    });
+
     form.insert({after:link});
     form.addClassName('disabled');
+  },
+
+  //Override to remove wrapper element as well
+  remove: function(){
+    this.element.up('.post-wrapper').remove();
+  },
+
+  //Disables the post, then removes after a short timeout (default 1 sec)
+  fancyRemove: function(timeout){
+    var post = this;
+    post.set('disabled', true);
+    setTimeout(function(){
+      post.remove();
+    }, timeout || 1000);
   }
 
 });
